94e60b25b84b136dedad6c87e9e706e824af05d6
lurauch <lukas.rauch@uni-kassel.de>
2024-11-09 11:56:48 +0100

    add slurm script

---
+++ slurm/xcl_pretrain.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/zsh
+#SBATCH --mem=200gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --gres=gpu:1
+#SBATCH --partition=main
+#SBATCH --job-name=birdMAE_pretrain_XCL
+#SBATCH --output=/mnt/work/bird2vec/logs/%x_%a.log
+#SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4
+#####SBATCH --nodelist=gpu-a100-5
+####SBATCH --array=3-3%3
+
+date;hostname;pwd
+source /mnt/home/lrauch/.zshrc
+#source ~/envs/gadme_v1/bin/activate
+echo Activate conda
+conda activate gadme_v1
+echo $PYTHONPATH
+
+cd /mnt/home/lrauch/projects/birdMAE/
+
+export CUDA_LAUNCH_BLOCKING=1
+export HYDRA_FULL_ERROR=1
+
+SEED=$SLURM_ARRAY_TASK_ID
+
+srun python train_ssl.py experiment=pretrain_xcl.yaml
+
+echo "Finished script."
