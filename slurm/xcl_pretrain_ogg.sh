8933688504cdab6d2fbf058836b4dacc14c908da
lurauch <lukas.rauch@uni-kassel.de>
2024-11-20 16:06:36 +0100

    small fix

--- slurm/xcl_pretrain_ogg.sh
+++ slurm/xcl_pretrain_ogg.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=main
 #SBATCH --job-name=birdMAE_pretrain_XCL_scratch_mgpu_ogg
 #SBATCH --output=/mnt/work/bird2vec/logs/mgpu_ogg.log
-#SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4, gpu-a100-1, gpu-a100-2
+#SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4,gpu-a100-1,gpu-a100-2
 #####SBATCH --nodelist=gpu-a100-5
 ####SBATCH --array=3-3%3
 
