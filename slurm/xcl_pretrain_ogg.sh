4dff7f51386fa8de128a9567df8d04e21d68934c
lurauch <lukas.rauch@uni-kassel.de>
2024-11-21 13:49:34 +0100

    adjust to 4 gpus across nodes

--- slurm/xcl_pretrain_ogg.sh
+++ slurm/xcl_pretrain_ogg.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/zsh
 #SBATCH --mem=200gb
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=28
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --partition=main
-#SBATCH --job-name=birdMAE_pretrain_XCL_scratch_mgpu_ogg
-#SBATCH --output=/mnt/work/bird2vec/logs/mgpu_ogg.log
+#SBATCH --nodes=1-4
+#SBATCH --job-name=birdMAE_pretrain_XCL_scratch_mgpu_ogg4
+#SBATCH --output=/mnt/work/bird2vec/logs/mgpu_ogg4.log
 #SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4
+#SBATCH --time=48:00:00  # Specify a 2-hour time limit
+
 ######,gpu-a100-1,gpu-a100-2
 #####SBATCH --nodelist=gpu-a100-5
 ####SBATCH --array=3-3%3
