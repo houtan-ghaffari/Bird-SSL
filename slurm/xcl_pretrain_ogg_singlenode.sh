bd2d5db1be776540c2786b28b0244757da112d33
lurauch <lukas.rauch@uni-kassel.de>
2024-11-30 18:50:49 +0100

    resume birdspec

--- slurm/xcl_pretrain_ogg_singlenode.sh
+++ slurm/xcl_pretrain_ogg_singlenode.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/zsh
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=26
-#SBATCH --gres=gpu:4
-#SBATCH --mem=128gb
+#SBATCH --gres=gpu:2
+#SBATCH --mem=164gb
 #SBATCH --partition=main
-#SBATCH --job-name=birdMAE_pretrain_XCL_scratch_mgpu_birdset
-#SBATCH --output=/mnt/work/bird2vec/logs/mgpu_birdset_spec_%N_%t.log
-#SBATCH --time=60:00:00
+#SBATCH --job-name=birdMAE_pretrain_XCL_scratch_mgpu_birdset_res
+#SBATCH --output=/mnt/work/bird2vec/logs/mgpu_birdset_spec_%N_%t_res.log
+#SBATCH --time=96:00:00
 ###SBATCH --exclude=gpu-v100-3
-#SBATCH --nodelist=gpu-l40s-1
+#SBATCH --nodelist=gpu-a100-5
 
 ###SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4
 ######,gpu-a100-1,gpu-a100-2
@@ -34,6 +34,8 @@ hostname
 srun python train_ssl.py experiment=pretrain_xcl_wave.yaml \
         trainer.devices=4 \
         +trainer.num_nodes=1 \
-        trainer.precision=bf16
+        trainer.precision=bf16 \
+        ckpt_path="/mnt/work/bird2vec/logs_pretrain_audioset_MAE/pretrain_xcl_wave/runs/XCL/AudioMAE/2024-11-29_132014/callback_checkpoints/last.ckpt"
+
 
 echo "Finished script."
