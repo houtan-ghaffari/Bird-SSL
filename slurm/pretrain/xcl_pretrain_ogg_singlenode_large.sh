2bb0971d657467056210c4c1d71ef3460fcf7307
lurauch <lukas.rauch@uni-kasel.de>
2024-12-15 00:24:13 +0100

    prepare base swin v2

--- slurm/pretrain/xcl_pretrain_ogg_singlenode_large.sh
+++ slurm/pretrain/xcl_pretrain_ogg_singlenode_large.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/zsh
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=26
-#SBATCH --gres=gpu:2
-#SBATCH --mem=500gb
+#SBATCH --gres=gpu:4
+#SBATCH --mem=400gb
 #SBATCH --partition=main
-#SBATCH --job-name=birdMAE_XCL_swinv2_large_nomix_stable
-#SBATCH --output=/mnt/work/bird2vec/logs/birdMAE_XCL_swinv2_large_%N_%t_nomix_stable.log
+#SBATCH --job-name=birdMAE_XCL_swinv2_large_0.5mix_stable_
+#SBATCH --output=/mnt/work/bird2vec/logs/birdMAE_XCL_swinv2_large_%N_%t_0.5mix_stable_.log
 #SBATCH --time=96:00:00
-#SBATCH --nodelist=gpu-l40s-1,gpu-a100-1,gpu-a100-2,gpu-a100-3,gpu-a100-5
+#SBATCH --nodelist=gpu-l40s-1
 
-####SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4,gpu-a100-4
+#####SBATCH --exclude=gpu-v100-1,gpu-v100-2,gpu-v100-3,gpu-v100-4,gpu-a100-4,gpu-a100-1
 
 ######,gpu-a100-1,gpu-a100-2
 ####SBATCH --array=3-3%3
@@ -30,10 +30,21 @@ export HYDRA_FULL_ERROR=1
 hostname
 srun python train_ssl.py \
         experiment=pretrain_xcl_wave_large.yaml \
-        trainer.devices=2 \
+        trainer.devices=4 \
         +trainer.num_nodes=1 \
         trainer.precision=16-mixed \
-        data.transform.waveform_augmentations.mixup_wave.p=0.0
+        trainer.strategy=ddp_find_unused_parameters_true \
+        data.transform.waveform_augmentations.mixup_wave.p=0.5 \
+        trainer.max_epochs=50 
+        ##ckpt_path="/mnt/work/bird2vec/logs_pretrain_audioset_MAE/pretrain_xcl_large_swin/runs/XCL/AudioMAE/2024-12-12_162203/callback_checkpoints/last.ckpt"
         #ckpt_path="/mnt/work/bird2vec/logs_pretrain_audioset_MAE/pretrain_xcl_wave_large/runs/XCL/AudioMAE/2024-11-23_123703/callback_checkpoints/last.ckpt"
+
+if [ $? -ne 0 ]; then
+    echo "Error: srun failed. Sleeping for 2 hours..."
+    sleep 6h
+    # Optionally, exit non-zero to signal failure
+    exit 1
+fi
+
 
 echo "Finished script."
